0d4b1846426f62fd1b1fb0c8114cdd568c40683a
ChillyWilly <noreply>
2021-08-04 16:33:46 -0400

    unfinished file structure

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <div>
+                Arizona Legal
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
